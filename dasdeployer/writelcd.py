e9aa736d0d95deca5c4531cee4c6f99887779d05
Martin Woodward <martin@woodwardweb.com>
2019-05-09 09:54:32 +0100

    Add ability to switch off display from script

--- dasdeployer/writelcd.py
+++ dasdeployer/writelcd.py
@@ -3,13 +3,20 @@
 import argparse
 from lcd import LCD_HD44780_I2C
 
-parser = argparse.ArgumentParser(description='Write a message to the LCD matrix display.')
-parser.add_argument('message', type=str,
-                   help='Text to write to the display')
+parser = argparse.ArgumentParser(
+    description='Write a message to the LCD matrix display.', 
+    epilog='Example: ./writelcd.py $\'Hello\\nWorld!\'')
+parser.add_argument('message', help='Text to write to the display')
+parser.add_argument('--displayOff', dest='display', action='store_false', default=True, help='Turn off the display')
 
 args = parser.parse_args()
 
 lcd = LCD_HD44780_I2C()
-lcd.message = args.message
+if (args.display):
+    lcd.message = args.message
+else:
+    lcd.clear(False)
+
+
 
 
