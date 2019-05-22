ffdc0f734bb0cc0425d7c718b3ce67ffcf07d9e0
Martin Woodward <martin@woodwardweb.com>
2019-05-22 23:02:53 +0100

    Add demo mode

--- dasdeployer/dasdeployer.py
+++ dasdeployer/dasdeployer.py
@@ -6,6 +6,7 @@ from signal import pause
 from time import sleep
 from lcd import LCD_HD44780_I2C
 from rgb import Color, RGBButton
+from datetime import datetime
 
 import socket
 
@@ -23,6 +24,8 @@ leds = LEDBoard(switchLight, toggleLight)
 lcd = LCD_HD44780_I2C()
 rgbmatrix = RGBButton()
 bigButton = Button(17)
+buildNumber = ""
+activeEnvironment = "Dev"
 
 ## Nifty get_ip function from Jamieson Becker https://stackoverflow.com/a/28950776
 def get_ip():
@@ -48,41 +51,133 @@ def reboot():
     leds.off()
     check_call(['sudo', 'reboot'])
 
+def demo_release_toggle():
+    lcd.message = "{}\nContosoAir {}\nBuild Successful.".format(TITLE,buildNumber)
+    rgbmatrix.fillButton(Color.GREEN)
+    rgbmatrix.stopRing()
+    bigButton.when_pressed = None
+
+def demo_dev_deploy():
+    demo_deploy_question("Dev")
+def demo_stage_deploy():
+    demo_deploy_question("Staging")
+def demo_prod_deploy():
+    demo_deploy_question("Prod")
+
+def demo_deploy_question(location):
+    activeEnvironment = location
+    lcd.message = "{}\nContosoAir\n{}\nDeploy to {}?".format(TITLE,buildNumber,activeEnvironment)
+    rgbmatrix.pulseButton(Color.RED, 1)
+    rgbmatrix.unicornRing(25)
+    bigButton.when_pressed = demo_deploy
+    return activeEnvironment
+
+def demo_deploy():
+    bigButton.when_pressed = None
+    rgbmatrix.fillButton(Color.WHITE)
+    rgbmatrix.chaseRing(Color.BLUE, 1)
+    lcd.message = "{}\nContosoAir\n{}\nDeploying to {}...".format(TITLE,buildNumber,activeEnvironment)
+    if activeEnvironment == "Dev":
+        toggle.dev.when_released = None
+    elif activeEnvironment == "Staging":
+        toggle.stage.when_released = None
+    elif activeEnvironment == "Prod":
+        toggle.prod.when_released = None
+    sleep(10)
+    rgbmatrix.fillButton(Color.GREEN)
+    rgbmatrix.stopRing()
+    lcd.message = "{}\nContosoAir {}\nDeploy Successful.".format(TITLE,buildNumber)
+    if activeEnvironment == "Dev":
+        toggleLight.dev.off()
+    elif activeEnvironment == "Staging":
+        toggleLight.stage.off()
+    elif activeEnvironment == "Prod":
+        toggleLight.prod.off()
+
+def demo():
+    # Demo mode
+    leds.off()
+    rgbmatrix.off()    
+    switch.green.when_pressed = None
+    switch.orange.when_pressed = None
+    switch.red.when_pressed = None
+    switch.red.when_held = run_diagnostics
+    lcd.message = TITLE
+
+    sleep(3)
+    rgbmatrix.fillButton(Color.GREEN)
+    sleep(3)
+
+    buildNumber = "{:%Y%m%d.%M}".format(datetime.now())
+
+    # Building
+    lcd.message = "{}\nBuilding ContosoAir\n\n{}...".format(TITLE,buildNumber) 
+    rgbmatrix.pulseButton(Color.GREEN, 1)
+    rgbmatrix.chaseRing(Color.BLUE, 1)
+    sleep(10)
+
+    # Good build
+    lcd.message = "{}\nContosoAir\n{}\nBuild Successful.".format(TITLE,buildNumber)
+    rgbmatrix.fillButton(Color.GREEN)
+    rgbmatrix.pulseRing(Color.WHITE, 1)
+    sleep(2)
+    rgbmatrix.stopRing()
+    
+    toggle.dev.when_pressed = demo_dev_deploy
+    toggle.stage.when_pressed = demo_stage_deploy
+    toggle.prod.when_pressed = demo_prod_deploy
+ 
+    toggle.dev.when_released = demo_release_toggle
+    toggle.stage.when_released = demo_release_toggle
+    toggle.prod.when_released = demo_release_toggle
+
+    sleep(5)
+    toggleLight.dev.on()
+    sleep(1)
+    toggleLight.stage.on()
+    sleep(1)
+    toggleLight.prod.on()
+
 def run_diagnostics():
     """ Diagnostic menu when Red button is held down """
     cpu = CPUTemperature()
     lcd.message = TITLE + \
         "\nIP:  " + get_ip() + \
         "\nCPU: " + str(round(cpu.temperature)) + chr(0xDF) + \
-        "\nOff  Reset      Back" 
+        "\nOff  Reset Demo Back" 
+    switchLight.on()
+    
     switch.red.wait_for_release()
     switch.red.when_held = None
+    
     switch.red.when_pressed = shutdown
     switch.orange.when_pressed = reboot
+    switch.green.when_pressed = demo
+
     switch.blue.wait_for_press()
+
+    switchLight.off()
+    
+    switch.green.when_pressed = None
     switch.orange.when_pressed = None
     switch.red.when_pressed = None
     switch.red.when_held = run_diagnostics
     lcd.message = TITLE
 
-# Attach diagnotic menu to red button when held down
-switch.red.when_held = run_diagnostics
+def main():
+    # Attach diagnotic menu to red button when held down
+    switch.red.when_held = run_diagnostics
 
-# Quick init sequence to show all is well
-lcd.message = TITLE + "\n\n\n" + get_ip()
-leds.blink(0.5,0.5,0,0,2,True)
-rgbmatrix.pulseButton(Color.RED, 1)
-rgbmatrix.unicornRing(25)
-sleep(3)
-rgbmatrix.off()
+    # Quick init sequence to show all is well
+    lcd.message = TITLE + "\n\n\n" + get_ip()
+    leds.blink(0.5,0.5,0,0,2,True)
+    rgbmatrix.pulseButton(Color.RED, 1)
+    rgbmatrix.unicornRing(25)
+    sleep(4)
+    rgbmatrix.off()
 
-lcd.message = TITLE
+    lcd.message = TITLE
 
-switch.orange.when_pressed = rgbmatrix.pulseButton
-switch.green.when_pressed = rgbmatrix.chaseRing
+    pause()
 
-switch.blue.when_pressed = rgbmatrix.stopRing
-switch.red.when_pressed = rgbmatrix.stopButton
-switch.blue.when_held = rgbmatrix.off
-
-pause()
+main()
